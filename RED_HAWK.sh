4d7dde7bd82dc4cc7eabec99002bb25e3d631dc9
HEXER22 <40181915+HEXER22@users.noreply.github.com>
2018-06-13 19:02:38 +0530

    Add files via upload

---
+++ RED_HAWK.sh
@@ -0,0 +1,22 @@
+apt-get update && apt-get upgrade -y && apt-get dist-upgrade -y
+
+pkg install php -y 
+
+pkg install git -y
+
+git clone https://github.com/Tuhinshubhra/RED_HAWK.git
+
+chmod +x RED_HAWK
+
+cd RED_HAWK
+
+chmod +x rhawk.php
+
+echo " php rhawk.php to execute RED_HAWK"
+
+echo "Thank You for using HEXER22 Scripts"
+
+
+
+
+
